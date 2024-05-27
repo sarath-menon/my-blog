547ad155f29445735305fa440761f8d89051ddfe
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-27 10:54:11 +0200

    added generateStaticParams for sttaic redering of dynamic routes

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,13 +2,36 @@
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import { promises as fs } from "fs";
 import * as matter from 'gray-matter';
+import { compareDesc } from "date-fns";
 
+export async function generateStaticParams() {
+  const fs = require("fs");
+  const path = require("path");
+  const contentDirectory = path.join(process.cwd(), "app/content");
+  const filenames = fs.readdirSync(contentDirectory);
+
+  const posts = filenames
+  .map((filename: string) => {
+    const filePath = path.join(contentDirectory, filename);
+    const { data, content } = matter.read(filePath);
+    return { ...data, content, fileName: filename };
+  })
+  .filter((post: { published: boolean }) => {
+    return post.published;
+  })
+  .sort((a: { date: string }, b: { date: string }) => {
+    return compareDesc(new Date(a.date), new Date(b.date));
+  });
+ 
+  return posts.map((post) => ({
+    slug: post.fileName,
+  }))
+}
 
 export default async function BlogPage({ params }: { params: { slug: string } }) {
 
   const filePath = process.cwd() + '/app/content/' + params.slug;
   const mdxSource = matter.read(filePath);
-  
 
   return (<div className="prose dark:prose-invert">
     <MDXRemote source={mdxSource.content} /></div>)
