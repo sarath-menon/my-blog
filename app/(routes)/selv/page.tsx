2ccf2611099f83b76c24d57a87058dfdff9d1b95
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-27 09:24:34 +0200

    added grematter pkg to parse mdx metadata

--- app/(routes)/selv/page.tsx
+++ app/(routes)/selv/page.tsx
@@ -3,11 +3,15 @@ import { promises as fs } from "fs";
 import * as matter from 'gray-matter';
  
 export default async function RemoteMdxPage() {
-  const source = await fs.readFile(
-    process.cwd() + "/app/content/test.mdx",
-    "utf8"
-  );
+  // const source = await fs.readFile(
+  //   process.cwd() + "/app/content/test.mdx",
+  //   "utf8"
+  // );
 
-  return <div className="prose dark:prose-invert"><MDXRemote source={source} /></div>
+  const mdxSource = matter.read(process.cwd() +'/app/content/test.mdx');
+
+  console.log("selv mdxSource.excerpt", mdxSource.data);
+
+  return <div className="prose dark:prose-invert"><MDXRemote source={mdxSource.content} /></div>
 }
 
