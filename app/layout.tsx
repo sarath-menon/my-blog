09ab669a5421d3e8f34506f34d52d45116fee707
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-27 12:55:34 +0200

    moved css files to styles folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 
-import "./globals.css";
+import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = FontSans({
