b0147b9b919760ffc6e33ab4c9df254cbdbe190e
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-06-02 17:02:03 +0200

    added code, video, paper links

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { formatDate } from "@/lib/utils";
 import { PROJECTS, ProjectInfo } from "@/config/projects";
+import { CodeIcon, GithubIcon, ScrollTextIcon, VideoIcon } from "lucide-react";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export default async function ProjectPage() {
   return (
@@ -17,28 +19,59 @@ export default async function ProjectPage() {
       </div>
       <hr className="my-8" />
 
-        <div className="grid gap-12 sm:grid-flow-row">
-          {PROJECTS.map((project: ProjectInfo) => (
-            <article
-              key={project.title}
-              className="group relative flex flex-col "
-            >
-              <div className="cursor-pointer space-y-2">
-                <span className="text-3xl font-semibold" >{project.title}</span>
+      <div className="grid gap-12 sm:grid-flow-row">
+        {PROJECTS.map((project: ProjectInfo) => (
+          <article
+            key={project.title}
+            className="group relative flex flex-col "
+          >
+            <div className="cursor-pointer space-y-2">
+              <span className="text-3xl font-medium">{project.title}</span>
 
-                {project.description && (
-                  <p className="text-muted-foreground text-lg">{project.description}</p>
+              {project.description && (
+                <p className="text-muted-foreground text-lg">
+                  {project.description}
+                </p>
+              )}
+
+              {/* {project.date && (
+                <p className="text-sm text-muted-foreground">
+                  {formatDate(project.date)}
+                </p>
+              )} */}
+
+              <div className="flex items-center gap-8">
+                {project.code && (
+                  <a
+                    href={project.code}
+                    className="flex items-center gap-2 text-muted-foreground"
+                  >
+                    <GitHubLogoIcon className="w-4 h-4" /> Code
+                  </a>
                 )}
 
-                {project.date && (
-                  <p className="text-sm text-muted-foreground">
-                    {formatDate(project.date)}
-                  </p>
+                {project.video && (
+                  <a
+                    href={project.video}
+                    className="flex items-center gap-1 text-muted-foreground"
+                  >
+                    <VideoIcon className="w-4 h-4" /> Video
+                  </a>
+                )}
+
+                {project.paper && (
+                  <a
+                    href={project.paper}
+                    className="flex items-center gap-1 text-muted-foreground"
+                  >
+                    <ScrollTextIcon className="w-4 h-4" /> Paper
+                  </a>
                 )}
               </div>
-            </article>
-          ))}
-        </div>
+            </div>
+          </article>
+        ))}
+      </div>
     </div>
   );
 }
